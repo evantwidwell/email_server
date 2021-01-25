b4aee46b23f4922ecc385d4551a8ce13c9fbae29
evantwidwell <evanski@gmail.com>
2021-01-25 10:22:53 -0600

    insert users

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/user');
 require('./services/passport');
-require('./models/user')
-
 
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
