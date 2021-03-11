6b3f066472dfd49788e606623dde6e004331ba4b
evantwidwell <evanski@gmail.com>
2021-03-11 10:34:57 -0600

    finding bugs

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,15 @@
 //figure out which set of credentials to return
-
-if(process.env.NODE_ENV === 'production'){
-  // we are in production - return the prod keys
-  module.exports = require('./prod')
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
 } else {
-  //we are in development - return the dev keys!!
-  module.exports = require('./dev')
+  // we are in development - return the dev keys!!!
+  module.exports = require('./dev');
 }
+// if(process.env.NODE_ENV === 'production'){
+//   // we are in production - return the prod keys
+//   module.exports = require('./prod')
+// } else {
+//   //we are in development - return the dev keys!!
+//   module.exports = require('./dev')
+// }
--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,7 @@ passport.deserializeUser((id, done) => {
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
-  clientSecret: keys.googleClentSecret,
+  clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
 }, (accessToken, refreshToken, profile, done) => {
