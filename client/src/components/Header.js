45be140b0c864d84ebd19fd1b4d666e5e8d218da
evantwidwell <evanski@gmail.com>
2021-03-19 11:49:09 -0500

    adding header features

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent(){
+    switch(this.props.auth){
+      case null:
+        return;
+      case false:
+        return  <li><a href="/auth/google">Login with Google</a></li>
+        
+      default:
+        return <li><a href="/api/logout">Logout</a></li>
+    }
+  }
   render() {
     return(
       <nav>
       <div className="nav-wrapper">
-        <a href="/surveys" className="left brand-logo">Emaily</a>
+        <Link to ={ this.props.auth ? '/surveys':'/'}
+        className="left brand-logo"
+        >Emaily</Link>
         <ul  className="right ">
-          <li><a>Login with Google</a></li>
+          {this.renderContent()}
         </ul>
       </div>
     </nav>
     )
   }
 };
+function mapStateToProps ({ auth }) {
+  return { auth }
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
