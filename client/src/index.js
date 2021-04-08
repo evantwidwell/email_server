c12b172f9ebb6eef7bf8d344c28ea28044870bc0
evantwidwell <evanski@gmail.com>
2021-04-08 09:48:04 -0500

    adding in survey sending

--- client/src/index.js
+++ client/src/index.js
@@ -19,5 +19,5 @@ ReactDOM.render(
 document.querySelector('#root')
 );
 
-console.log('Stripe key is ', process.env.REACT_APP_STRIPE_KEY)
-console.log('Environment is', process.env.NODE_ENV)
+// console.log('Stripe key is ', process.env.REACT_APP_STRIPE_KEY)
+// console.log('Environment is', process.env.NODE_ENV)
